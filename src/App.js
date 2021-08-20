6b87e286af7f6ad27ab5d57d713a89d8f1d6ca67
heejaykong <heejay.dev@gmail.com>
2021-08-21 01:04:54 +0900

    debug deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
+import {HashRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import Home from './route/Home'
 import MyProfile from './route/MyProfile'
 import MyGarden from './route/MyGarden';
-
 import './style/App.css';
 
 class App extends React.Component {
   render(){
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <Switch>
-        <Route path='/madCamp_week3' component={Home} />
-        <Route path='/' exact component={Home} />
+        <Route exact path='/' component={Home} />
         <Route path='/profile' component={MyProfile} />
         <Route path='/mygarden' component={MyGarden} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 }
